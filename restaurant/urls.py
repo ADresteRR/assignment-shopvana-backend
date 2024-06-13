401c57d51dc805159626f7c74a7864c6594c433c
ADresteRR <adresterrjr001@gmail.com>
2024-06-13 14:59:01 +0530

    add root endpoint

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -19,7 +19,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+def serverCheck():
+    print('server is running fine')
 urlpatterns = [
+    path("/", serverCheck),
     path('orders/', include('orders.urls')),
     path('admin/', admin.site.urls),
 ]
